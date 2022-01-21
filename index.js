c72e5880a3a3c2163663afabf1065d65f06b82a9
Matthieu Grunnagel <mouamem2edunom@gmail.com>
2022-01-21 11:08:12 +0000

    ...

--- index.js
+++ index.js
@@ -2,26 +2,37 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ ws: { intents: new Discord.Intents(Discord.Intents.ALL) } });
 
 const express = require('express');
-const es6Renderer = require('express-es6-template-engine')
 const app = express();
 const path = require('path');
 const router = express.Router();
 const port = 3000;
 
-// const cons = require('consolidate');
-
 const date = require('./tools/date.js');
 const c = require('./tools/colors.js');
 
+const entrePotes = {
+  id: '901541669726474340',
+  Général: { id: '901541669726474343' }
+}
+const Dev = {
+  id: '898885394714734613',
+  Général: { id: '898885395293564991' }
+}
+activeGl = Dev.id
+activeCh = Dev.Général.id
+
+const cInput = () => {
+  
+}
+
 client.on('ready', async () => {
-  console.log('Ready!');
-  let guild = client.guilds.cache.get('901541669726474340');
-  let channel = guild.channels.cache.get('901541669726474343');
-  let messages = await channel.messages.fetch();
-  messages = Array.from(messages.values());
-  let conv = []; 
+  let guild = client.guilds.cache.get(activeGl);
+  let channel = guild.channels.cache.get(activeCh);
+  let messages = await channel.messages.fetch(); messages = Array.from(messages.values());
+  let conv = [];
   messages.reverse().forEach(i => {
-    let name = `@${i.member?i.member.displayName:i.author.name}${i.author.bot?' [bot]':''}`;
+    if (!i.member) { return }
+    let name = `@${i.member ? i.member.displayName : i.author.name}${i.author.bot ? ' [bot]' : ''}`;
     // TODO: Virer les pseudo et le timestamp si deux messages d'affilée
     let html = `
       <h5><span style="color: ${i.member.displayHexColor}">${name}</span> [${date(i.createdTimestamp)}]</h4>
@@ -30,30 +41,54 @@ client.on('ready', async () => {
     console.log(message);
     conv.push(html);
   });
+  conv.reverse();
   router.get('/', (req, res) => {
     res.render('pages/index', {
       guild: guild.name,
       channel: channel.name,
-      messages: conv.reverse()
-    })
-  })
+      messages: conv
+    });
+  });
   router.get('/about', (req, res) => {
     res.render('pages/about')
-  })
-
-  // app.use(express.favicon(path.join(__dirname, 'icon.png')));
-  // app.set('views', path.join(__dirname, 'views'));
+  });
+  app.use(express.static(__dirname + '/public'));
   app.set('view engine', 'ejs');
   app.use('/', router);
-  app.listen(port || 3000 );
-  /*
-  // const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
-  await client.guilds.cache.get('901541669726474340').channels.cache.get('901541669726474343').send('test');
-  //*/
+  app.listen(port || 3000);
+  // ╭╮╰╯─│┌┐└┘─│┏┓┗┛━┃
+
+  process.openStdin().addListener('data', async input => {
+    await client.guilds.cache.get(activeGl).channels.cache.get(activeCh).send(input.toString().trim());
+    let rows = '─'.repeat(50)
+    let spaces = ' '.repeat(50)
+    let box = '┌' + rows + '┐' + '\n│' + spaces + '│\n' + '└' + rows + '┘'
+    process.stdout.write(box)
+    process.stdout.moveCursor(-50, -1)
+  });
+
+  let rows = '─'.repeat(50)
+  let spaces = ' '.repeat(50)
+  let box = '┌' + rows + '┐' + '\n│' + spaces + '│\n' + '└' + rows + '┘'
+  process.stdout.write(box)
+  process.stdout.moveCursor(-50, -1)
+
+  process.stdout.on('resize', function() {
+    let rows = '─'.repeat(process.stdout.rows - 2)
+    let box = '┌' + rows + '┐'
+    process.stdout.write(box)
+  })
 });
 
-client.on('message', i => {
-  let name = `@${i.member?i.member.displayName:i.author.name}${i.author.bot?' [bot]':''}`; 
+client.on('message', async i => {
+  process.stdout.moveCursor(0, 0)
+  process.stdout.clearLine();
+  process.stdout.moveCursor(0, -1)
+  process.stdout.clearLine();
+  process.stdout.moveCursor(0, -1)
+  process.stdout.clearLine();
+  if (!i.member) { return }
+  let name = `@${i.member ? i.member.displayName : i.author.name}${i.author.bot ? ' [bot]' : ''}`;
   let message = `${i.guild.name}/#${i.channel.name} - ${c(`<${name}>`, i.member.displayHexColor)} [${date(i.createdTimestamp)}]\n${i.content}\n`;
   console.log(message);
 });
