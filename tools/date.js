08fb6293cb5cbcc4c2b86c792e3fe6b9716fa88c
NoAccount1 <mouamem2edunom@gmail.com>
2022-01-24 20:02:53 +0100

    Switching to typescript and cleaning up the mess

--- tools/date.js
+++
@@ -1,9 +0,0 @@
-module.exports = (t) => {
-  let d = new Date(t);
-  return dformat = [d.getMonth()+1,d.getDate(),d.getFullYear()].join('/')+' '+[d.getHours(),d.getMinutes(),d.getSeconds()].join(':');
-}
-
-
-// console.log(date(1641207489207))
-
-module.exports
