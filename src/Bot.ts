bfbe03801a5cee97a403b4cdfb7b6c5bc7e97e94
NoAccount1 <mouamem2edunom@gmail.com>
2022-01-24 20:32:31 +0100

    Restarting from scratch with a clean TypeScript

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,1 +1,9 @@
-import { Client } from "discord.js"
+import { Client } from "discord.js";
+const client = new Client();
+require("dotenv").config();
+
+client.on('ready', async () => {
+  console.log(`Logged as ${client.user?.username}`)
+})
+
+client.login(process.env.TOKEN);
